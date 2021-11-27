7ea5d6ccbcdaf61085d48a1699245330a1da410b
rommel667 <rommel667@gmail.com>
2021-11-27 22:11:25 +0800

    edit container to trigger github actions

--- container/src/App.js
+++ container/src/App.js
@@ -3,6 +3,9 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
     return (
-       <MarketingApp />
+        <div>
+            <h1>Hi there</h1>
+            <MarketingApp />
+        </div>
     )
 }
