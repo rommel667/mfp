3fde9f7be7c91999556006b6c1a24bc47860f41a
rommel667 <rommel667@gmail.com>
2021-11-29 10:32:16 +0800

    update container header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
     return (
         <div>
-            <h1>Hi there!!!</h1>
+            <Header />
             <MarketingApp />
         </div>
     )
