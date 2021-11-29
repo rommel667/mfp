8444705bb9f3cd2f53d86c92b12dc0ff16f76713
rommel667 <rommel667@gmail.com>
2021-11-29 12:57:28 +0800

    generateClassName

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp'
 
+const generatedClassName = createGenerateClassName({
+    productionPrefix: "co"
+})
+
 export default () => {
     return (
-        <div>
+        <StylesProvider generateClassName={generatedClassName}>
             <Header />
             <MarketingApp />
-        </div>
+        </StylesProvider>
     )
 }
