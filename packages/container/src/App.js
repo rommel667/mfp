6abfc34107e569447a46cb0a624e7c21dc549668
rommel667 <rommel667@gmail.com>
2021-11-29 11:58:40 +0800

    test change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
+// import Header from './components/Header';
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
+            <h1>Hi there</h1>
             <MarketingApp />
         </div>
     )
